d18141ebb1aeedd3224a2036f0a21dfce81d5552
裴刚委 <peigangwei2016@gmail.com>
2016-09-27 23:07:44 +0800

    learn textview

--- Learn.java
+++ Learn.java
@@ -1,6 +1,12 @@
 public class Learn{
 
 	public static void main (Stirng[] args){
+
+		textViewLearn();
 		System.out.println("hello word");
+	}
+
+	private void textViewLearn(){
+		//TextVIew学习代码
 	}
 }
