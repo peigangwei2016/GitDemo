e953dff79dbbe7c66ce3726c06993ec52670479a
裴刚委 <peigangwei2016@gmail.com>
2016-09-27 23:50:58 +0800

    learn button

--- Learn.java
+++ Learn.java
@@ -3,10 +3,15 @@ public class Learn{
 	public static void main (Stirng[] args){
 
 		textViewLearn();
+		buttonLearn();
 		System.out.println("hello word");
 	}
 
 	private void textViewLearn(){
 		//TextVIew学习代码
+	}
+
+	private void buttonLearn(){
+		// Button 学习的代码
 	}
 }
